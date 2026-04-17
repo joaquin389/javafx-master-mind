e9b76ce8caae71fa730a2a63889e47a82689574f
joaquin <joaquin.cozette@ynov.com>
2026-04-17 14:31:59 +0200

    la j'ai fais le master mind et avancé le mec qui parle

--- src/main/java/com/example/javafxmastermindcontrole/HelloApplication.java
+++ src/main/java/com/example/javafxmastermindcontrole/HelloApplication.java
@@ -10,10 +10,14 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("mastermind.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 500);
+        stage.setTitle("Mastermind - Démineur");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
